a1cb6ca88f8c09b7c6f6b19c2ca5ba0986948c63
subikshabamboo <mangaikug@gmail.com>
2026-01-30 15:27:29 +0530

    Documentation for Go backend

--- frontend_1/test/widget_test.dart
+++ frontend_1/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:frontend_1/screens/trace_fully.dart';
+import 'package:frontend_1/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
